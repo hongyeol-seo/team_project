23a37d7e64687a390fef82a458cdab9cfc37e3e4
hong <napolehong92@naver.com>
2023-01-13 10:52:39 +0900

    blog_home_change

--- django_project1/urls.py
+++ django_project1/urls.py
@@ -23,7 +23,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('blog/', include('blog.urls'))
+    path('blog/', include('blog.urls')),
+    path('', include('blog.urls'))
+    
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
